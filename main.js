f3a0d679ff7212f758ae531494e997d617c9a11e
hajdur <hajdu.reka1109@gmail.com>
2023-10-24 15:53:26 +0200

    random generator

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-var strength;
-var dexterity;
-var constitution;
-var intelligence;
-var wisdom;
-var charisma;
+var strength = 0;
+var dexterity = 0;
+var constitution = 0;
+var intelligence = 0;
+var wisdom = 0;
+var charisma = 0;
+
+$(".container").on("click", ".roll", (event) => {
+	// Update stats and HTML elements
+	strength = rollStats();
+	dexterity = rollStats();
+	constitution = rollStats();
+	intelligence = rollStats();
+	wisdom = rollStats();
+	charisma = rollStats();
+
+	// Update HTML elements
+	document.getElementById("str").textContent = strength;
+	document.getElementById("dex").textContent = dexterity;
+	document.getElementById("cons").textContent = constitution;
+	document.getElementById("inte").textContent = intelligence;
+	document.getElementById("wis").textContent = wisdom;
+	document.getElementById("charism").textContent = charisma;
+});
 
 function rollStats(stat) {
 	var stat = Math.random();
